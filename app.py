939e4b4fedf6d3748684f97ef51d03a02311c880
marlon <brendonalavancagem@gmail.com>
2025-09-01 20:31:13 -0300

    Railway: add Procfile, runtime.txt e ajustar porta

--- app.py
+++ app.py
@@ -233,4 +233,9 @@ def download():
 
 if __name__ == "__main__":
     print("Servidor iniciado… http://127.0.0.1:5000/")
-    app.run(debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
